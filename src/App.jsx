160a21ded05a10b2add9f062c37f106551e17c1b
André de Arruda <andre23arruda@gmail.com>
2023-08-30 23:29:23 -0400

    feat: New watchPosition options

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,34 @@ import './App.css'
 
 
 export default function App() {
-	const [speed, setSpeed] = useState(0)
+	const [speed, setSpeed] = useState((0).toFixed(1))
 	const [isStopped, setIsStopped] = useState(true)
-
-	let watchID = null
-	const options = {enableHighAccuracy:true}
+	const [watchID, setWatchID] = useState(null)
+	const options = {
+		enableHighAccuracy: true,
+		timeout: 5000,
+		maximumAge: 0,
+	}
 
 	function onClick() {
 		if (!watchID && isStopped) {
-			watchID = navigator.geolocation.watchPosition(
+			setWatchID(navigator.geolocation.watchPosition(
 				updatePosition, handleError, options
-			)
+			))
 			setIsStopped(false)
 		}
 		else {
 			navigator.geolocation.clearWatch(watchID)
-			watchID = null
-			setSpeed(0)
+			setWatchID(null)
+			setSpeed((0).toFixed(1))
 			setIsStopped(true)
 		}
 	}
 
 	function updatePosition(position) {
+		if (isStopped) {
+			setSpeed((0).toFixed(1))
+		}
 		if (position.coords.speed === null)
 			return
 		setSpeed((position.coords.speed * 3.6).toFixed(1))
